d39766ab196780c9e2daea00f2f9b693c7657ed2
solomiia <solomiiagulitashvili@gmail.com>
2019-10-13 19:09:09 +0300

    fetch

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -7,7 +7,7 @@
       return newArr;
     });
 
-    oneArray.push([{ v: 7, f: '' }, null]);
+    oneArray.push([{ v: 7, f: "" }, null]);
 
     return oneArray;
   };
@@ -15,26 +15,43 @@
     alert(errorMessage);
   };
 
+  // window.network = {
+  //   load: function(onLoad, address, value) {
+  //     var URL = '/api/' + address + '/' + value;
+  //     var xhr = new XMLHttpRequest();
+  //     xhr.responseType = 'json';
+
+  //     xhr.addEventListener('load', function() {
+  //       if (xhr.status === 200) {
+  //         console.log(xhr.response);
+  //         var transformedData = transformData(xhr.response);
+  //         onLoad(transformedData);
+  //       } else {
+  //         onError(xhr.status + ' ' + xhr.statusText);
+  //       }
+  //     });
+  //     xhr.addEventListener('error', function(errorMessage) {
+  //       onError(errorMessage);
+  //     });
+  //     xhr.open('GET', URL);
+  //     xhr.send();
+  //   },
+  // };
   window.network = {
     load: function(onLoad, address, value) {
-      var URL = '/api/' + address + '/' + value;
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function() {
-        if (xhr.status === 200) {
-          console.log(xhr.response);
-          var transformedData = transformData(xhr.response);
-          onLoad(transformedData);
-        } else {
-          onError(xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function(errorMessage) {
-        onError(errorMessage);
-      });
-      xhr.open('GET', URL);
-      xhr.send();
-    },
+      let URL = "/api/" + address + "/" + value;
+      fetch(URL)
+        .then(response => response.json())
+        .then(
+          result => {
+            let transformedData = transformData(result);
+            onLoad(transformedData);
+          },
+          reject => {
+            let errorMessage = reject;
+            onError(errorMessage);
+          }
+        );
+    }
   };
 })();
